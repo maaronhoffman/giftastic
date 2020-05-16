ff3540594e633ae8305f925ad096860021a90a5c
Michael Hoffman <maaronhoffman@gmail.com>
2020-05-16 12:48:04 -0700

    cleaning up and added some fixes and btn fix

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,8 +11,8 @@ function drawButtons(){
     for (var i = 0; i < topics.length; i++){
 
         var $button = $("<button>") 
-        $button.attr("class", "btn btn-primary m-3");
-        $button.attr("id", "clickButton")  
+        $button.attr("class", "btnClick btn btn-primary m-3");
+        $button.attr("id", "btnClick")  
         $button.attr("data-name", topics[i]); 
         $button.text(topics[i]); 
         $("#buttons").append($button); 
@@ -23,7 +23,7 @@ function drawButtons(){
 
 
 function drawImages() {
-    
+    event.preventDefault();
     $("#gifHolder").empty();
     var $userInput = $("#userInput").val().trim();
     var limit = 10;
@@ -37,7 +37,7 @@ function drawImages() {
             for(let i = 0; i < limit; i++) {    
 
                 var $imgDiv = $("<div>");
-                $imgDiv.addClass("imgHolder");
+                $imgDiv.addClass("imgHolder justify-content-center");
             
                 var $img = $("<img>");
                 $img.attr("src", response.data[i].images.fixed_height_still.url);
@@ -57,6 +57,52 @@ function drawImages() {
         });
     }
 
+    function buttonImg() {
+    
+
+        event.preventDefault();
+
+        $("#gifHolder").empty();
+        var $userInput = $(this).attr("data-name");
+        var limit = 10;
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + $userInput + "&limit=" + limit + "&api_key=d0lrXwwVl7CbDKBVCj3IDQy2xrEkjOgU";
+    
+        $.ajax({
+                url: queryURL, 
+                method: "GET"
+            }).done(function(response) {
+    
+                for(let i = 0; i < limit; i++) {    
+    
+                    var $imgDiv = $("<div>");
+                    $imgDiv.addClass("imgHolder justify-content-center");
+                
+                    var $img = $("<img>");
+                    $img.attr("src", response.data[i].images.fixed_height_still.url);
+                    $img.attr("still", response.data[i].images.fixed_height_still.url);
+                    $img.attr("animate", response.data[i].images.fixed_height.url);
+                    $img.attr("state", "still");
+                    $img.attr("class", "gif justify-content-center");
+                    $imgDiv.append($img);
+    
+                    var rating = response.data[i].rating;
+                    var $rating = $("<p>").text("Rating: " + rating);
+                    $imgDiv.append($rating)
+    
+                    $("#gifHolder").append($imgDiv);
+                    console.log($imgDiv);
+                }
+            });
+        }
+
+
+
+
+
+
+
+
+    
 function swapState() {          
 
     var state = $(this).attr("state");
@@ -75,15 +121,20 @@ function swapState() {
     }
 
 $("#inputSubmit").on("click",function(){
-
-    var $userInput = $("#userInput").val().trim();
     event.preventDefault();
-    console.log($userInput);
+    var $userInput = $("#userInput").val().trim();
+    
+   
     topics.push($userInput);
+    
     drawButtons();
     drawImages();
+    
+    
+    
 });
 
 drawButtons();
 $("#gifHolder").on("click", ".gif", swapState);
+$("#buttons").on("click", ".btn", buttonImg);
 });
